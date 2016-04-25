78754346a23836f1b3476115228703bfb0fc1035
Davuzk NA <spiritrootkit@gmail.com>
2016-04-25 21:35:43 +0200

    Added files via upload

---
+++ passwd_sec.c
@@ -0,0 +1,50 @@
+/*
+* Password secure in C.
+* Purpose: Prevent buffer overflow attack.
+* Author: Davuzk
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct
+{
+	char pass[200];	
+} passwd;
+
+int main(int argc, char *argv[])
+{
+	int rootaccess_privileges = 0;
+	
+	if(argc != 2)
+		fprintf(stderr,"Usage: %s <password>\n", argv[0]);
+	if(argc != 2)
+		exit(-1);
+	passwd *pw;
+	pw = malloc(sizeof(passwd));
+	
+	if(abs == NULL)
+		exit(-1);
+	
+	pw = (passwd *) memset(pw, 0, sizeof(passwd));
+	
+	memcpy((char*) (pw->pass), argv[1], 20);  
+	
+	if(!strncmp(pw->pass, "takecookies123", sizeof(pw->pass)))
+	{
+		printf("You got the right password!!\n");
+		rootaccess_privileges = 1;
+	}
+		
+	else
+	{
+		printf("Wrong password\n");
+	}
+	
+	if(rootaccess_privileges)
+		printf("Root access privileges given to the user!\n");
+	
+	return 0;
+}
