40218b6ca9080674163f917642088d6531ee7b18
Davuzk NA <spiritrootkit@gmail.com>
2016-05-31 21:15:53 +0200

    Add files via upload

---
+++ gaia.s
@@ -0,0 +1,201 @@
+	.file	"gaia.c"
+	.section	.rodata
+	.align 8
+.LC0:
+	.string	"Usage: %s [target_host] [start port] [end port]\n"
+	.text
+	.globl	program_usage
+	.type	program_usage, @function
+program_usage:
+.LFB2:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movq	%rdi, -8(%rbp)
+	movq	stderr(%rip), %rax
+	movq	-8(%rbp), %rdx
+	movl	$.LC0, %esi
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	fprintf
+	movl	$0, %edi
+	call	exit
+	.cfi_endproc
+.LFE2:
+	.size	program_usage, .-program_usage
+	.section	.rodata
+	.align 8
+.LC1:
+	.string	"\n[+] Starting scan at: %s port: %d end port: %d\n\n"
+	.align 8
+.LC2:
+	.string	"[-] Port: %d/%d is closed at host: %s\n\n"
+	.align 8
+.LC3:
+	.string	"[+] Port: %d/%d is open at host: %s\n\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB3:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$48, %rsp
+	movl	%edi, -36(%rbp)
+	movq	%rsi, -48(%rbp)
+	cmpl	$4, -36(%rbp)
+	je	.L3
+	movq	-48(%rbp), %rax
+	movq	(%rax), %rax
+	movq	%rax, %rdi
+	call	program_usage
+.L3:
+	movl	$24, %edi
+	call	malloc
+	movq	%rax, -8(%rbp)
+	movl	$0, %edx
+	movl	$1, %esi
+	movl	$2, %edi
+	call	socket
+	movl	%eax, %edx
+	movq	-8(%rbp), %rax
+	movl	%edx, 8(%rax)
+	movq	-8(%rbp), %rax
+	movl	8(%rax), %eax
+	cmpl	$1, %eax
+	jne	.L4
+	movl	$1, %edi
+	call	exit
+.L4:
+	movq	-48(%rbp), %rax
+	movq	8(%rax), %rdx
+	movq	-8(%rbp), %rax
+	movq	%rdx, (%rax)
+	movq	-48(%rbp), %rax
+	addq	$16, %rax
+	movq	(%rax), %rax
+	movq	%rax, %rdi
+	call	atoi
+	movl	%eax, %edx
+	movq	-8(%rbp), %rax
+	movl	%edx, 12(%rax)
+	movq	-48(%rbp), %rax
+	addq	$24, %rax
+	movq	(%rax), %rax
+	movq	%rax, %rdi
+	call	atoi
+	movl	%eax, %edx
+	movq	-8(%rbp), %rax
+	movl	%edx, 16(%rax)
+	movq	-8(%rbp), %rax
+	movl	16(%rax), %ecx
+	movq	-8(%rbp), %rax
+	movl	12(%rax), %edx
+	movq	-48(%rbp), %rax
+	addq	$8, %rax
+	movq	(%rax), %rax
+	movq	%rax, %rsi
+	movl	$.LC1, %edi
+	movl	$0, %eax
+	call	printf
+	movq	-8(%rbp), %rax
+	movl	12(%rax), %edx
+	movq	-8(%rbp), %rax
+	movl	%edx, 20(%rax)
+	jmp	.L5
+.L10:
+	movq	-8(%rbp), %rax
+	movl	12(%rax), %edx
+	movq	-8(%rbp), %rax
+	movl	%edx, 12(%rax)
+	jmp	.L6
+.L9:
+	movq	-8(%rbp), %rax
+	movq	(%rax), %rax
+	movq	%rax, %rdi
+	call	inet_addr
+	movl	%eax, -28(%rbp)
+	movw	$2, -32(%rbp)
+	movq	-8(%rbp), %rax
+	movl	12(%rax), %eax
+	movzwl	%ax, %eax
+	movl	%eax, %edi
+	call	htons
+	movw	%ax, -30(%rbp)
+	movq	-8(%rbp), %rax
+	movl	8(%rax), %eax
+	leaq	-32(%rbp), %rcx
+	movl	$16, %edx
+	movq	%rcx, %rsi
+	movl	%eax, %edi
+	call	connect
+	testl	%eax, %eax
+	jns	.L7
+	movq	-8(%rbp), %rax
+	movq	(%rax), %rcx
+	movq	-8(%rbp), %rax
+	movl	16(%rax), %edx
+	movq	-8(%rbp), %rax
+	movl	12(%rax), %eax
+	movl	%eax, %esi
+	movl	$.LC2, %edi
+	movl	$0, %eax
+	call	printf
+	jmp	.L8
+.L7:
+	movq	-8(%rbp), %rax
+	movq	(%rax), %rcx
+	movq	-8(%rbp), %rax
+	movl	16(%rax), %edx
+	movq	-8(%rbp), %rax
+	movl	12(%rax), %eax
+	movl	%eax, %esi
+	movl	$.LC3, %edi
+	movl	$0, %eax
+	call	printf
+.L8:
+	movq	-8(%rbp), %rax
+	movl	12(%rax), %eax
+	leal	1(%rax), %edx
+	movq	-8(%rbp), %rax
+	movl	%edx, 12(%rax)
+.L6:
+	movq	-8(%rbp), %rax
+	movl	12(%rax), %edx
+	movq	-8(%rbp), %rax
+	movl	16(%rax), %eax
+	cmpl	%eax, %edx
+	jle	.L9
+	movq	-8(%rbp), %rax
+	movl	20(%rax), %eax
+	leal	1(%rax), %edx
+	movq	-8(%rbp), %rax
+	movl	%edx, 20(%rax)
+.L5:
+	movq	-8(%rbp), %rax
+	movl	20(%rax), %edx
+	movq	-8(%rbp), %rax
+	movl	16(%rax), %eax
+	cmpl	%eax, %edx
+	jle	.L10
+	movq	-8(%rbp), %rax
+	movl	8(%rax), %eax
+	movl	%eax, %edi
+	call	close
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE3:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 5.3.1 20160406 (Red Hat 5.3.1-6)"
+	.section	.note.GNU-stack,"",@progbits
